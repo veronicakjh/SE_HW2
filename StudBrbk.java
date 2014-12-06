f88d8d3ce3d42c9e8b33c6ef24f811a873a71f49
Veronica <cleopatra300@naver.com>
2014-12-06 20:44:54 +0900

    오늘마무리!할거얍

--- StudBrbk.java
+++ StudBrbk.java
@@ -1,21 +1,44 @@
 package Stud;
 
 import java.io.*;
+import java.sql.*;
 
 public class StudBrbk {
 
-	public void studbrbk() throws IOException{
+	StudBrbk() throws IOException{
+		try{
+				Class.forName("com.mysql.jdbc.Driver");
+		}
+		catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void studbrbk() throws IOException,SQLException{
+		
+		String bname, author, publisher, availability, renting_stud;
+		int ISBN;
+		availability="";
+		renting_stud="";
 		
 		InputStreamReader sr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(sr);
 		
-		for(int i=1;i==10;i++){//여기서 i는 sql로 검색이된 숫자
-			System.out.println(i+". 도서명 ");
-			System.out.println("저자: ");
-			System.out.println("출판사: ");
-			System.out.println("ISBN: ");
-			System.out.println("대출가능여부: ");
-			System.out.println("대여자: ");
-		}
+		String jdbcUrl="jdbc:mysql://127.0.0.1:3306/library";
+		String userID = "root";
+		String userPass="1111";
+		
+		Connection conn = DriverManager.getConnection(jdbcUrl, userID, userPass);
+		PreparedStatement ps= null;
+		String sql=null;
+		ResultSet rs = null;
+		 
+		System.out.println("---------------------------");
+		System.out.println("자신이 대여 중인 도서 목록");
+		System.out.println("---------------------------");
+		
+		// 여기부터 오늘 11시
+		
+		
 	}
 }
